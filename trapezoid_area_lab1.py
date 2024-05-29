9027e42ed66c9331bf4d4e03dff3e53e493dd425
aksunk01 <111587552+aksunk01@users.noreply.github.com>
2024-05-29 13:11:46 -0400

    Add files via upload
    This was the first lab in CSE 120 Intro to programming with Python

---
+++ trapezoid_area_lab1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  1 09:04:54 2023
+
+@author: aksun
+"""
+
+if __name__ == '__main__':
+    #Values of bases and heights
+    base1 = 5
+    base2 = 7
+    height = 10
+    #area calculated
+    area = 0.5 * (base1 + base2) * height
+    
+    #Print statement for each base, height, and area
+    print("Base 1 is", base1, "\nBase 2 is", base2, "\nHeight is", height, "\nArea is", area)
